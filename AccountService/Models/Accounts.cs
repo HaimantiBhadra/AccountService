80bc2ee39505e146e1a4ab800275dbcaa68fe285
HaimantiBhadra <67627518+HaimantiBhadra@users.noreply.github.com>
2020-07-01 17:30:53 +0530

    Update Accounts.cs

--- AccountService/Models/Accounts.cs
+++ AccountService/Models/Accounts.cs
@@ -7,7 +7,15 @@ namespace AccountService.Models
 {
     public class Accounts
     {
-        public int ID { get; set; }
+         public int ID { get; set; }
         public string AccountType { get; set; }
+
+        public string  Name { get; set; }
+
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PAN { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
     }
 }
