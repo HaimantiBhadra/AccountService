84032290bf6f3dcb2382188ad112413c68d51ddc
Haimanti Bhadra <haimanti.bhadra@gmail.com>
2020-07-01 16:06:01 +0530

    AccountService

---
+++ AccountService/Models/Accounts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountService.Models
+{
+    public class Accounts
+    {
+        public int ID { get; set; }
+        public string AccountType { get; set; }
+    }
+}
